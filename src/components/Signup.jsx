804d60cdc5485de5c9c5e68ca77bab054ed27286
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-05-11 20:07:34 +0300

    L-271. Return enteredValues from signupAction and set them as default values to fields so that they are not cleared if an error exists

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -41,17 +41,29 @@ export default function Signup() {
       errors.push('Please select a role.');
     }
 
-    if (!terms) {
-      errors.push('You must agree to the terms and conditions.')
-    }
-
     if (acquisitionChannel.length === 0) {
       errors.push('Please select at least one acquisition channel.');
     }
 
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.')
+    }
+
     if (errors.length > 0) {
       // we can return anything
-      return { errors };
+      return {
+        errors,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisitionChannel,
+          terms,
+        }
+      };
     }
 
     return { errors: null };
@@ -82,13 +94,23 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email} // '?' -> email will be accessed, only if formState.enteredValues is defined. Returns undefined otherwise
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password}
+          />
         </div>
 
         <div className="control">
@@ -97,6 +119,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -106,18 +129,32 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role} // unfortunately role comboBox resets after submit. Not even Max knows how to fix it.
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -134,6 +171,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -144,20 +182,32 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('other')}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultChecked={formState.enteredValues?.terms}
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
@@ -170,6 +220,7 @@ export default function Signup() {
       )}
 
       <p className="form-actions">
+        {/* Reset button resets fields to default values. Since we set different default values, reset does not clear fields. */}
         <button type="reset" className="button button-flat">
           Reset
         </button>
